634fba28eaa1949aeb5594fe43cfbf5419ba2ff6
sudoQ <vorga88@gmail.com>
2018-03-17 21:11:34 +0200

    add logo, animate logo

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function () { 
+    // var logo = document.querySelector(".logo__image");
+
+    // logo.addEventListener('mouseover', (e) => {
+    //     logo.setAttribute('src', 'img/mishka-logo-hover.png');
+    // });
+
+    // logo.addEventListener('mouseout', (e) => {
+    //     logo.setAttribute('src', 'img/mishka-logo-desktop.png');
+    // });
+
+
+
+
+});
