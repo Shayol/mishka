bbfc7f93e6ce408645bc3a3c08e1c70e0449fb9f
sudoQ <vorga88@gmail.com>
2018-04-08 17:51:58 +0300

    add config for multiple files to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     rules: [
       {
         test: /\.html$/,
-        exclude: /index\.html$/, //without this HtmlWebPackPlugin will not work with template file correctly
+        exclude: /(index|catalog|form)\.html$/, //without this HtmlWebPackPlugin will not work with template file correctly
         use: [
           {
             loader: "html-loader",
@@ -79,9 +79,19 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      title: 'Title',
+      title: 'index',
       template: "./src/html/index.html",
       filename: "./index.html"
+    }),
+    new HtmlWebPackPlugin({
+      title: 'catalog',
+      template: "./src/html/catalog.html",
+      filename: "./catalog.html"
+    }),
+    new HtmlWebPackPlugin({
+      title: 'form',
+      template: "./src/html/form.html",
+      filename: "./form.html"
     }),
     new ExtractTextPlugin('style.css')
   ]
