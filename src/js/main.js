95edc7201c9e3a63008ea91da1538607862fd2bf
sudoQ <vorga88@gmail.com>
2018-04-21 12:22:12 +0300

    opacity for product name on hover and click

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,5 @@
 import "../scss/main.scss";
 
-// window.addEventListener('load', function () {
+window.addEventListener('load', function () {
 
-//     var products = document.querySelector(".products-list__content");
-
-
-//     var data = [{
-//         image: '../img/product1.png',
-//         name: "Зайчик-попрыгайчик",
-//         info: "Рост 30 см, вес 200 г",
-//         price: "1 200 руб."
-//     },
-//     {
-//         image: '../../img/product2.png',
-//         name: "Корзинка для белья",
-//         info: "Диаметр 15 см, высота 10 см",
-//         price: "690 руб."
-//     },
-//     {
-//         image: '../../img/product3.png',
-//         name: "Большая корзинка для игрушек",
-//         info: "Диаметр 30 см, высота 30 см",
-//         price: "1 500 руб."
-//     }];
-
-//     data.forEach(item => {
-//         products.innerHTML += `<a href="#" class="products-list__item product">
-//                             <div class="product__img"></div>
-//                             <div class="product__description">
-//                                 <div class="product__name">${item.name}</div>
-//                                 <div class="product__info">${item.info}</div>
-//                             </div>
-//                             <div class="product__footer">
-//                                 <div class="product__price">${item.price}</div>
-//                                 <a href="#" class="product__cart cart">
-//                                     <span class="cart__icon"></span>
-//                                 </a>
-//                             </div>
-//                         </a>`
-
-//     });
-// });
+});
