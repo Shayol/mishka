0b842a158d7e7e01881cef01612c38f99521c080
sudoQ <vorga88@gmail.com>
2018-04-26 00:58:07 +0300

    add transition to mobile menu and fix top-menu links visibility on desktop

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,15 @@ window.addEventListener('load', function () {
     NodeList.prototype.forEach = Array.prototype.forEach;
 
     var menu = document.querySelector('.top-menu__menu');
-    var menuIcon = document.querySelector('.menu__icon');
-    var menuClose = document.querySelector('.menu__close');
+    var menuIcon = document.querySelector('.menu__svg');
+    var menuClose = document.querySelector('.menu__close-svg');
     var mobileMenu = document.querySelector('.mobile-menu');
     var topMenu = document.querySelector('.top-menu');
 
     menu.addEventListener('click', (event) => {
         topMenu.classList.toggle('top-menu--mobile');
-        menuIcon.classList.toggle('menu__icon--invisible');
-        menuClose.classList.toggle('menu__close--visible');
+        menuIcon.classList.toggle('menu__svg--invisible');
+        menuClose.classList.toggle('menu__close-svg--visible');
         mobileMenu.classList.toggle('mobile-menu--visible');
 
         
