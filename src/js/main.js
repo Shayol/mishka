763063547d4a2aac406ba77a20fa013afe87a699
sudoQ <vorga88@gmail.com>
2018-04-25 23:09:09 +0300

    add mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,20 @@
 import "../scss/main.scss";
 
 window.addEventListener('load', function () {
+    NodeList.prototype.forEach = Array.prototype.forEach;
 
+    var menu = document.querySelector('.top-menu__menu');
+    var menuIcon = document.querySelector('.menu__icon');
+    var menuClose = document.querySelector('.menu__close');
+    var mobileMenu = document.querySelector('.mobile-menu');
+    var topMenu = document.querySelector('.top-menu');
+
+    menu.addEventListener('click', (event) => {
+        topMenu.classList.toggle('top-menu--mobile');
+        menuIcon.classList.toggle('menu__icon--invisible');
+        menuClose.classList.toggle('menu__close--visible');
+        mobileMenu.classList.toggle('mobile-menu--visible');
+
+        
+    });
 });
