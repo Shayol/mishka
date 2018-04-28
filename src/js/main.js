f03ef18799ff36f4b6e4e7f115917dfa4e330073
sudoQ <vorga88@gmail.com>
2018-04-28 19:50:51 +0300

    add positioning and hiding showing on click for modal

--- src/js/main.js
+++ src/js/main.js
@@ -9,12 +9,32 @@ window.addEventListener('load', function () {
     var mobileMenu = document.querySelector('.mobile-menu');
     var topMenu = document.querySelector('.top-menu');
 
+    var addToCart = document.querySelectorAll(".js-add-to-cart");
+    var modal = document.querySelector(".modal");
+
     menu.addEventListener('click', (event) => {
         topMenu.classList.toggle('top-menu--mobile');
         menuIcon.classList.toggle('menu__svg--invisible');
         menuClose.classList.toggle('menu__close-svg--visible');
         mobileMenu.classList.toggle('mobile-menu--visible');
 
-        
+
+    });
+
+    addToCart.forEach(el => {
+        el.addEventListener('click', (event) => {
+            modal.style.display = 'block';
+            var modalWindow = modal.querySelector('.modal__window');
+            var height = modalWindow.style.height.slice(0,-2);
+            modalWindow.style.top = Math.round(window.pageYOffset) + "px";
+        });
+    });
+
+
+    modal.addEventListener('click', (event) => {
+        if (event.target.classList.contains('modal') || event.target.classList.contains('modal__button')) {
+            event.preventDefault();
+            modal.style.display = 'none';
+        }
     });
 });
