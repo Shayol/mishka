4face18d40f9717e25fc1b4e6499f5a68755abac
mila <vorga88@google.com>
2018-10-10 15:26:20 +0300

    change packes to new versions and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,61 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const postcssPresetEnv = require('postcss-preset-env');
 
-module.exports = {
-  entry: './src/js/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'script.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 9000
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
+module.exports =
+  {
+    entry: './src/js/main.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'script.js'
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      port: 9000
+    },
+    devtool: "source-map",
+    module: {
+      rules: [{
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
         }
       },
       {
         test: /\.html$/,
-        exclude: /(index|catalog|form)\.html$/, //without this HtmlWebPackPlugin will not work with template file correctly
-        use: [
-          {
-            loader: "html-loader",
-            options: {
-              attrs: ['img:src', 'source:srcset'], //allows to use picture tag
-              minimize: true
-            }
+        exclude: /(index|catalog|form)\.html$/, //without this the underscore template won't be executed.
+        include: path.join(__dirname, 'src/html'),
+        use: [{
+          loader: "html-loader",
+          options: {
+            minimize: true,
+            interpolate: true,
+            attrs: ['img:src', 'source:srcset']
           }
-        ]
+        }]
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [{
-            loader: "css-loader",
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
             options: {
-              minimize: true,
-              importLoaders: 1,
-              sourceMap: true
-            },
+              importLoaders: 1
+            }
           },
           {
             loader: 'postcss-loader',
             options: {
-              ident: 'postcss',
               sourceMap: true,
-              plugins: [
-                require('postcss-cssnext')()
+              ident: 'postcss',
+              plugins: () => [
+                require("autoprefixer")()
               ]
             }
           },
@@ -62,11 +65,10 @@ module.exports = {
               sourceMap: true
             }
           }
-          ]
-        })
+        ]
       },
       {
-        test: /\.(eot|woff|ttf)$/,
+        test: /\.(eot|woff|woff2|ttf)$/,
         use: {
           loader: "file-loader",
           options: {
@@ -84,25 +86,27 @@ module.exports = {
             outputPath: 'img/'
           }
         }
-      }
+      },
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        title: 'index',
+        template: "./src/html/index.html",
+        filename: "./index.html"
+      }),
+      new HtmlWebPackPlugin({
+        title: 'catalog',
+        template: "./src/html/catalog.html",
+        filename: "./catalog.html"
+      }),
+      new HtmlWebPackPlugin({
+        title: 'form',
+        template: "./src/html/form.html",
+        filename: "./form.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      title: 'index',
-      template: "./src/html/index.html",
-      filename: "./index.html"
-    }),
-    new HtmlWebPackPlugin({
-      title: 'catalog',
-      template: "./src/html/catalog.html",
-      filename: "./catalog.html"
-    }),
-    new HtmlWebPackPlugin({
-      title: 'form',
-      template: "./src/html/form.html",
-      filename: "./form.html"
-    }),
-    new ExtractTextPlugin('style.css')
-  ]
-};
+  };
